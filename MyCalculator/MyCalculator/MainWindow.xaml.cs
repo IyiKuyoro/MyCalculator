277dd60a3969e2742174701efd89785ba99bce5b
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2017-11-07 11:02:05 +0100

    Calculator MainWindow

--- MyCalculator/MyCalculator/MainWindow.xaml.cs
+++ MyCalculator/MyCalculator/MainWindow.xaml.cs
@@ -20,9 +20,64 @@ namespace MyCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Fields
+        #endregion Fields
+
+        #region Properties
+        private bool IsON
+        {
+            get
+            {
+                return (bool)GetValue(IsONProperty);
+            }
+            set
+            {
+                SetValue(IsONProperty, value);
+            }
+        }
+        public string Status
+        {
+            get
+            {
+                return (string)GetValue(StatusProperty);
+            }
+            set
+            {
+                SetValue(StatusProperty, value);
+            }
+        }
+
+        #region DependencyProperties
+        public static DependencyProperty StatusProperty = DependencyProperty.Register("Status", typeof(string), typeof(MainWindow), new PropertyMetadata("Off"));
+        public static DependencyProperty IsONProperty = DependencyProperty.Register("IsON", typeof(bool), typeof(MainWindow), new PropertyMetadata(false));
+        #endregion DependencyProperties
+        #endregion Properties
+
+        #region Methods
+        #region EventHandlers
+        private void btnOnOff_Click(object sender, RoutedEventArgs e)
+        {
+            if (!IsON)
+            {
+                IsON = true;
+                Status = "On";
+            }
+            else
+            {
+                IsON = false;
+                Status = "Off";
+            }
+        }
+        #endregion EventHndlers
+        #endregion Methods
+
+        #region Constructors
         public MainWindow()
         {
+            IsON = false;
+            Status = "Off";
             InitializeComponent();
         }
+        #endregion Constructors
     }
 }
