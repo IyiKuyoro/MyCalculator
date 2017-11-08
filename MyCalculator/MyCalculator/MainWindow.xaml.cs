b63cb35686c4b764c3afa8b9563f333a83f3398f
Opeoluwa Iyi-Kuyoro <opeoluwa.iyikuyoro@gmail.com>
2017-11-08 10:32:20 +0100

    My Calculator Version(1.0.0)

--- MyCalculator/MyCalculator/MainWindow.xaml.cs
+++ MyCalculator/MyCalculator/MainWindow.xaml.cs
@@ -8,7 +8,21 @@ namespace MyCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Enumertion
+        enum Operations
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide,
+            None
+        }
+        #endregion Enumeration
+
         #region Fields
+        bool executed = false;
+        Operations opertor = Operations.None;
+        double result = 0;
         #endregion Fields
 
         #region Properties
@@ -54,6 +68,20 @@ namespace MyCalculator
         #endregion Properties
 
         #region Methods
+        /// <summary>
+        /// This method checks if execution has already occured and resets all parameters based on outcome.
+        /// </summary>
+        private void CheckExecution()
+        {
+            if (executed == true)
+            {
+                //computation was completed refreash result.
+                result = 0;
+                DisplayText = "";
+                executed = false;
+            }
+        }
+
         #region EventHandlers
         private void btnOnOff_Click(object sender, RoutedEventArgs e)
         {
@@ -61,26 +89,33 @@ namespace MyCalculator
             {
                 IsON = true;
                 Status = "On";
-                DisplayText = "";
+
             }
             else
             {
                 IsON = false;
                 Status = "Off";
-                DisplayText = "";
             }
+            executed = false;
+            result = 0;
+            opertor = Operations.None;
+            DisplayText = "";
         }
 
         private void btnOne_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
-                DisplayText += "1"; 
+                DisplayText += "1";
             }
         }
 
         private void btnTwo_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
                 DisplayText += "2";
@@ -89,6 +124,8 @@ namespace MyCalculator
 
         private void btnThree_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
                 DisplayText += "3";
@@ -97,6 +134,8 @@ namespace MyCalculator
 
         private void btnFour_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
                 DisplayText += "4";
@@ -105,6 +144,8 @@ namespace MyCalculator
 
         private void btnFive_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
                 DisplayText += "5";
@@ -113,6 +154,8 @@ namespace MyCalculator
 
         private void btnSix_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
                 DisplayText += "6";
@@ -121,6 +164,8 @@ namespace MyCalculator
 
         private void btnSeven_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
                 DisplayText += "7";
@@ -129,6 +174,8 @@ namespace MyCalculator
 
         private void btnEight_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
                 DisplayText += "8";
@@ -137,6 +184,8 @@ namespace MyCalculator
 
         private void btnNine_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10)
             {
                 DisplayText += "9";
@@ -145,6 +194,8 @@ namespace MyCalculator
 
         private void btnZero_Click(object sender, RoutedEventArgs e)
         {
+            CheckExecution();
+
             if (DisplayText.Length < 10 && DisplayText.Length > 0)
             {
                 DisplayText += "0";
@@ -154,15 +205,26 @@ namespace MyCalculator
         private void btnClear_Click(object sender, RoutedEventArgs e)
         {
             DisplayText = DisplayText.Remove(DisplayText.Length - 1);
+
+            if (DisplayText == "-")
+            {
+                DisplayText = "";
+            }
         }
 
         private void btnClearAll_Click(object sender, RoutedEventArgs e)
         {
+            result = 0;
+            opertor = Operations.None;
             DisplayText = "";
+            executed = false;
         }
 
         private void btnPlusMin_Click(object sender, RoutedEventArgs e)
         {
+            if (DisplayText.Length == 0)
+                return;
+
             if (DisplayText[0] != '-')
             {
                 DisplayText = "-" + DisplayText; 
@@ -170,6 +232,121 @@ namespace MyCalculator
             else
             {
                 DisplayText = DisplayText.Remove(0, 1);
+            }
+        }
+        
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            DisplayText += ".";
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            opertor = Operations.Add;
+            if (!executed)
+            {
+                result += double.Parse(DisplayText);
+            }
+            else
+            {
+                executed = false;
+                result = double.Parse(DisplayText);
+            }
+            DisplayText = "";
+        }
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            opertor = Operations.Subtract;
+            if (!executed)
+            {
+                if (result == 0)
+                    result = double.Parse(DisplayText);
+                else
+                    result -= double.Parse(DisplayText);
+            }
+            else
+            {
+                executed = false;
+                result = double.Parse(DisplayText);
+            }
+            DisplayText = "";
+        }
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            opertor = Operations.Multiply;
+            if (!executed)
+            {
+                if (result == 0)
+                {
+                    result = double.Parse(DisplayText); 
+                }
+                else
+                {
+                    result *= double.Parse(DisplayText);
+                }
+            }
+            else
+            {
+                executed = false;
+                result = double.Parse(DisplayText);
+            }
+            DisplayText = "";
+        }
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            opertor = Operations.Divide;
+            if (!executed)
+            {
+                if (result == 0)
+                    result = double.Parse(DisplayText);
+                else
+                    result /= double.Parse(DisplayText);
+            }
+            else
+            {
+                executed = false;
+                result = double.Parse(DisplayText);
+            }
+            DisplayText = "";
+        }
+
+        private void btnEnter_Click(object sender, RoutedEventArgs e)
+        {
+            executed = true;
+            try
+            {
+                switch (opertor)
+                {
+                    case (Operations.Add):
+                        result += double.Parse(DisplayText);
+                        DisplayText = result.ToString();
+                        break;
+                    case (Operations.Subtract):
+                        result -= double.Parse(DisplayText);
+                        DisplayText = result.ToString();
+                        break;
+                    case (Operations.Multiply):
+                        result *= double.Parse(DisplayText);
+                        DisplayText = result.ToString();
+                        break;
+                    case (Operations.Divide):
+                        result /= double.Parse(DisplayText);
+                        DisplayText = result.ToString();
+                        break;
+                    default:
+                        DisplayText = result.ToString();
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                DisplayText = "Error!";
+                result = 0;
+                MessageBox.Show(ex.Message, "Error!", MessageBoxButton.OKCancel, MessageBoxImage.Exclamation);
+                IsON = false;
             }
         }
         #endregion EventHndlers
@@ -183,6 +360,5 @@ namespace MyCalculator
             InitializeComponent();
         }
         #endregion Constructors
-
     }
 }
